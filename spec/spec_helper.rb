98bdeee18ada4b01cb5b0f20eb7d7f02165b9d87
jcarley <jeff.carley@gmail.com>
2014-08-09 11:36:13 -0500

    Corrects the require syntax for pulling in the main ruby file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,4 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'support'))
 
 require 'pry'
-require 'Kohawk'
+require 'kohawk'
