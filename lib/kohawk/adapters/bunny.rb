40fe2a2209a3cdefcc49f741afe6f525573642d7
jcarley <jeff.carley@gmail.com>
2014-08-10 19:16:11 -0500

    Adds implementation for bunny adapter.

--- lib/kohawk/adapters/bunny.rb
+++ lib/kohawk/adapters/bunny.rb
@@ -4,6 +4,23 @@ module Kohawk
   module Adapters
     class Bunny
 
+      attr_reader :session
+
+      def connect(options)
+        if session.nil?
+          @session = Bunny.new(options)
+          session.start
+        end
+        session
+      end
+
+      def disconnect
+        unless session.nil?
+          session.stop
+          @session = nil
+        end
+      end
+
     end
   end
 end
