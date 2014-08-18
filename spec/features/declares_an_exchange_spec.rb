c8760890f4dd21b55d2067960b5c73487557a4de
jcarley <jeff.carley@gmail.com>
2014-08-18 16:05:27 -0500

    Adds expectations to integration test.

--- spec/features/declares_an_exchange_spec.rb
+++ spec/features/declares_an_exchange_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rest-client'
 
 describe 'An exchange defined in the routes file' do
 
@@ -24,7 +25,20 @@ describe 'An exchange defined in the routes file' do
     cli = Kohawk::CLI.new
     cli.start
 
-    binding.pry
+    url = 'http://dev:dev@127.0.0.1:15672/api/exchanges/local/exampleapp'
+    RestClient.get url, {:accept => :json} do |response, request, result, &block|
+      puts response.code
+      case response.code
+      when 200
+        body = JSON.load(response.body)
+        expect(body["name"]).to eql("exampleapp")
+        expect(body["vhost"]).to eql("local")
+        expect(body["durable"]).to be(true)
+        expect(body["auto_delete"]).to be(true)
+      end
+    end
+
+    # binding.pry
   end
 
 end
